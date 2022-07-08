28a859aff9bd39b1b64347381a3353fcd63219ce
BBongKim <kbg0099@naver.com>
2022-07-08 20:28:57 +0900

    RecordRecyclerAdapter 추가

---
+++ app/src/main/java/com/bbongkim/projectrecord/record/RecordRecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package com.bbongkim.projectrecord.record
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bbongkim.projectrecord.database.RecordEntity
+import com.bbongkim.projectrecord.databinding.RecordItemLayoutBinding
+
+class RecordRecyclerAdapter : RecyclerView.Adapter<RecordRecyclerAdapter.RecordViewHolder>() {
+    // TODO item layout 완성, BindingAdapter, ViewModel 추가 필요
+    private val recordList = ArrayList<RecordEntity>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecordViewHolder {
+        val binding =
+            RecordItemLayoutBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return RecordViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: RecordViewHolder, position: Int) {
+        holder.bind(recordList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return recordList.size
+    }
+
+    inner class RecordViewHolder(private val binding: RecordItemLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(recordEntity: RecordEntity) {
+            binding.record = recordEntity
+        }
+    }
+}
